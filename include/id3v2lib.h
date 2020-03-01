9274472741a5b0f59c8065ac3b4ae5ca994ae8fb
choi-inkyun <dingpong@naver.com>
2020-03-01 16:47:29 +0900

    Add Private Frame Insertion Code

--- include/id3v2lib.h
+++ include/id3v2lib.h
@@ -37,6 +37,7 @@ ID3v2_frame* tag_get_comment(ID3v2_tag* tag);
 ID3v2_frame* tag_get_disc_number(ID3v2_tag* tag);
 ID3v2_frame* tag_get_composer(ID3v2_tag* tag);
 ID3v2_frame* tag_get_album_cover(ID3v2_tag* tag);
+ID3v2_frame* tag_get_private_data(ID3v2_tag* tag);
 
 // Setter functions
 void tag_set_title(char* title, char encoding, ID3v2_tag* tag);
@@ -51,6 +52,7 @@ void tag_set_disc_number(char* disc_number, char encoding, ID3v2_tag* tag);
 void tag_set_composer(char* composer, char encoding, ID3v2_tag* tag);
 void tag_set_album_cover(const char* filename, ID3v2_tag* tag);
 void tag_set_album_cover_from_bytes(char* album_cover_bytes, char* mimetype, int picture_size, ID3v2_tag* tag);
+void tag_set_private_data(char* private_data, char* owner_identifier, ID3v2_tag* tag);
 
 #ifdef __cplusplus
 } // end of extern C
