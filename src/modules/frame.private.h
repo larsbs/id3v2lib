7be8a73fc66324f338157eeaeb9442ac7992b772
Lars Ruiz <lars.bs@hotmail.com>
2022-11-18 02:36:01 +0100

    idem.

--- src/modules/frame.private.h
+++ src/modules/frame.private.h
@@ -13,8 +13,8 @@
 #include "modules/frame.h"
 
 ID3v2_frame* frame_parse(const char* buffer, int id3_major_version);
-ID3v2_text_frame_data* text_frame_data_parse(const char* buffer, int length);
-ID3v2_comment_frame_data* comment_frame_data_parse(const char* buffer, int length);
-ID3v2_apic_frame_data* apic_frame_data_parse(const char* buffer, int length);
+ID3v2_text_frame_data* text_frame_data_parse(const char* buffer, int frame_size);
+ID3v2_comment_frame_data* comment_frame_data_parse(const char* buffer, int frame_size);
+ID3v2_apic_frame_data* apic_frame_data_parse(const char* buffer, int frame_size);
 
 #endif
